92a1aec46c33fc561f671035d6c6329b7c12fa09
rlupo06 <rlupo07@gmail.com>
2020-03-08 16:32:10 -0500

    adding project

---
+++ src/main/groovy/com/lupo/gradle/plugin/GradlePlugin.groovy
@@ -0,0 +1,14 @@
+package com.lupo.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GradlePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.configure(project, {
+            Docker
+        })
+    }
+
+}
